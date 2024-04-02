a1d19d86054160cd88df3f28dddb5be54336d91a
woodyjacques <Woodyjacques1@gmail.com>
2024-04-01 20:52:54 -0400

    Agregando datos

--- src/components/headerSesion.tsx
+++ src/components/headerSesion.tsx
@@ -99,7 +99,15 @@ function HeaderSesion() {
                                 </NavLink>
                             </li>
                             <li>
-                                <a href="#"><button onClick={showModal} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Salir</button></a>
+                                <NavLink
+                                    to="#" onClick={showModal}
+                                    className={`block py-1 pl-2 pr-2 rounded ${location.pathname === "#"
+                                        ? "text-white"
+                                        : "md:hover:text-white"
+                                        } text-white bg-blue-600 hover:bg-blue-700 hover:text-white border-gray-700 my-1`}
+                                >
+                                    Salir
+                                </NavLink>
                             </li>
                         </ul>
                     </div>
