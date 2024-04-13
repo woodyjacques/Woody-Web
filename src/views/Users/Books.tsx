8e6ec6eac20e7aabba02839811a938ad0ca38f21
woodyjacques <woodyjacques1@gmail.com>
2024-04-12 23:16:58 -0400

    Cambios para google

--- src/views/Users/Books.tsx
+++ src/views/Users/Books.tsx
@@ -5,9 +5,38 @@ import Header from "../../components/header";
 import HeaderSesion from "../../components/headerSesion";
 import { useNavigate } from "react-router-dom";
 
-function Books() {
+export interface UserData {
+  name: string;
+  email: string;
+  paper: string;
+}
 
-  const token = localStorage.getItem("ACCESS_TOKEN");
+function Books() {
+  
+  const urlParams = new URLSearchParams(window.location.search);
+  const token = urlParams.get("token");
+  const name = urlParams.get("name");
+  const email = urlParams.get("email");
+  const paper = urlParams.get("paper");
+
+  if (token) {
+    localStorage.setItem("ACCESS_TOKEN", token);
+  }
+
+  if (email !== null || name !== null || paper !== null) {
+    const sessionData: UserData = {
+      name: name || '',
+      email: email || '',
+      paper: paper || '',
+    };
+
+    localStorage.setItem(
+      "USER_SESSION",
+      JSON.stringify(sessionData)
+    );
+  }
+
+  const tokens = localStorage.getItem("ACCESS_TOKEN");
   const paperes = localStorage.getItem("USER_SESSION");
 
   const navigate = useNavigate();
@@ -26,7 +55,7 @@ function Books() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900">
       <div className="flex-grow">
-        {token ? <HeaderSesion /> : <Header />}
+        {tokens ? <HeaderSesion /> : <Header />}
         <Section
           tittle="Libros"
           description="Bienvenido a mi colección de libros, donde la imaginación y el conocimiento se entrelazan para ofrecerte una experiencia única."
